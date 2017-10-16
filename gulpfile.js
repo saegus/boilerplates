d305272262b05ee720e305269373b92f295fa93b
Paturages <christian.ingouff@saegus.com>
2017-10-16 20:26:10 +0200

    Add main.scss entry point + basic JS support

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,25 @@ const PostCSS = require('gulp-postcss');
 const CSSNext = require('postcss-cssnext');
 const CleanCSS = require('gulp-clean-css');
 const Concat = require('gulp-concat');
+const Uglify = require('gulp-uglify');
 const BrowserSync = require('browser-sync').create();
 
-Gulp.task('sass', () => Gulp.src('./scss/**/*.scss')
+Gulp.task('sass', () => Gulp.src('./scss/main.scss')
   .pipe(Sass().on('error', Sass.logError))
   .pipe(PostCSS([ CSSNext() ]))
   .pipe(Concat('style.css'))
   .pipe(CleanCSS({ compatibility: 'ie10' }))
+  .pipe(Gulp.dest('./public'))
+  .pipe(BrowserSync.stream())
+);
+
+Gulp.task('js', () => Gulp.src([
+  // Example to add dependencies from npm:
+  require.resolve('lodash'),
+  './js/**/*.js',
+])
+  .pipe(Concat('bundle.js'))
+  .pipe(Uglify())
   .pipe(Gulp.dest('./public'))
   .pipe(BrowserSync.stream())
 );
@@ -21,7 +33,8 @@ Gulp.task('browser-sync', () => BrowserSync.init({
 
 Gulp.task('noop', () => undefined);
 
-Gulp.task('default', ['sass', 'browser-sync'], () => {
+Gulp.task('default', ['js', 'sass', 'browser-sync'], () => {
   Gulp.watch('./scss/**/*.scss', ['sass']);
   Gulp.watch('./public/**/*.html', ['noop']).on('change', BrowserSync.reload);
+  Gulp.watch('./js/**/*.js', ['js']);
 });
