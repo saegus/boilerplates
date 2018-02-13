eddecc1a4c45a057f6576427d57d52e01c3f9cb3
Paturages <christian.ingouff@saegus.com>
2018-02-13 11:33:25 +0100

    Condition PHP boilerplate for front-end dev

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,47 @@
 const Gulp = require('gulp');
-const ChildProcess = require('child_process');
-const exec = command =>
-  new Promise((resolve, reject) => ChildProcess.exec(command, (err) => {
-    if (err) reject(err);
-    else resolve();
-  }))
-;
+const Sass = require('gulp-sass');
+const PostCSS = require('gulp-postcss');
+const CSSNext = require('postcss-cssnext');
+const CleanCSS = require('gulp-clean-css');
+const Concat = require('gulp-concat');
+const Uglify = require('gulp-uglify');
+// const BrowserSync = require('browser-sync').create();
 
-Gulp.task('php', () =>
-  exec('./Transphpile/bin/transphpile transpile src --dest tmp')
-  .then(() => exec('rm -R dist'))
-  .then(() => exec('mv tmp/src dist'))
-  .then(() => exec('rmdir tmp'))
-  .catch(err => console.log(err))
+Gulp.task('sass', () => Gulp.src([
+  // Example to add CSS dependencies from npm (after doing `npm install bootstrap`):
+  // (to get the /dist/... path, just look into the corresponding "node_modules" folder)
+  require.resolve('bootstrap/dist/css/bootstrap.min.css'),
+  './scss/main.scss'
+])
+  .pipe(Sass().on('error', Sass.logError))
+  .pipe(PostCSS([CSSNext()]))
+  .pipe(Concat('style.css'))
+  .pipe(CleanCSS({ compatibility: 'ie10' }))
+  .pipe(Gulp.dest('./public'))
+  // .pipe(BrowserSync.stream())
 );
 
-Gulp.task('default', ['php'], () => {
-  Gulp.watch('./src/**/*.php', ['php']);
+Gulp.task('js', () => Gulp.src([
+  // Example to add dependencies from npm (after doing `npm install jquery` and `npm install bootstrap`):
+  // (to get the /dist/... path, just look into the corresponding "node_modules" folder)
+  require.resolve('jquery/dist/jquery.slim.min'),
+  require.resolve('bootstrap/dist/js/bootstrap'),
+  './js/**/*.js',
+])
+  .pipe(Concat('bundle.js'))
+  .pipe(Uglify())
+  .pipe(Gulp.dest('./public'))
+  // .pipe(BrowserSync.stream())
+);
+
+// Gulp.task('browser-sync', () => BrowserSync.init({
+//   server: { baseDir: 'public' }
+// }));
+
+// Gulp.task('noop', () => undefined);
+
+Gulp.task('default', ['js', 'sass'/*, 'browser-sync' */], () => {
+  Gulp.watch('./scss/**/*.scss', ['sass']);
+  // Gulp.watch('./public/**/*.html', ['noop']).on('change', BrowserSync.reload);
+  Gulp.watch('./js/**/*.js', ['js']);
 });
